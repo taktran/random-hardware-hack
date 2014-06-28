7500eeac309861e3c67a902c713533da91baf7f5
Tak Tran <contact@tutaktran.com>
2014-06-28 15:41:57 +0100

    Clean up front end code.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,20 +1,9 @@
 /*global Primus: true*/
 (function (){
   'use strict';
-  var gameInfo = {};
-
-  // Buttons
-  var startButtonEl = $('.start-button');
-  var restartButtonEl = $('.restart-button');
 
   // Containers
-  var initContainer = $('.init-container');
-  var gameContainer = $('.game-container');
-
-  // Game info
-  var timerEl = $('.timer');
-  var scoreEl = $('.score');
-  var gameOverModal = $('#gameOverModal');
+  // var resultsEl = $('.results');
 
   // ---------------------------------------
   // Primus connection
@@ -25,19 +14,17 @@
 
   primus.on('open', function open() {
     console.log('Connection open');
-
-    initGame();
   });
 
   primus.on('error', function error(err) {
     console.error('Error:', err, err.message);
   });
 
-  primus.on('reconnect', function () {
+  primus.on('reconnect', function() {
     console.log('Reconnect attempt started');
   });
 
-  primus.on('reconnecting', function (opts) {
+  primus.on('reconnecting', function(opts) {
     console.log('Reconnecting in %d ms', opts.timeout);
     console.log('This is attempt %d out of %d', opts.attempt, opts.retries);
   });
@@ -48,85 +35,10 @@
 
   primus.on('data', function(rawData) {
     var data = JSON.parse(rawData);
-    var messageType = data["type"];
-    var message = data["message"];
+    // var messageType = data["type"];
+    // var message = data["message"];
 
-    // console.log(data);
-
-    if (messageType === "timer") {
-      var timeLeft = message.timeLeft;
-      timerEl.text(timeLeft);
-    } else if (messageType === "info") {
-
-      gameInfo.timeLimit = message["timeLimit"];
-      timerEl.text(gameInfo.timeLimit);
-
-      gameInfo.score = message["score"];
-      scoreEl.text(gameInfo.score);
-    } else if (messageType === "score") {
-      gameInfo.score = message["score"];
-      scoreEl.text(gameInfo.score);
-    } else if (messageType === "gameOver") {
-      gameOverModal.modal();
-    } else if (messageType === "light") {
-      if (!message.lowLight) {
-        $("body").addClass("light");
-      } else {
-        $("body").removeClass("light");
-      }
-    }
+    console.log(data);
   });
 
-  // ---------------------------------------
-  // Game setup
-  // ---------------------------------------
-
-  function initGame() {
-
-    var gameState = {
-      init: function() {
-        initContainer.show();
-        gameContainer.hide();
-        gameOverModal.modal('hide');
-
-        primus.write({
-          type: "gameState",
-          message: "init"
-        });
-      },
-
-      start: function() {
-        initContainer.hide();
-        gameContainer.show();
-
-        primus.write({
-          type: "gameState",
-          message: "start"
-        });
-      },
-
-      restart: function() {
-        gameState.init();
-
-        primus.write({
-          type: "gameState",
-          message: "restart"
-        });
-      }
-    };
-
-    // ---------------------------------------
-    // Events
-    // ---------------------------------------
-
-    startButtonEl.click(function() {
-      gameState.start();
-    });
-
-    restartButtonEl.click(function() {
-      gameState.restart();
-    });
-
-    gameState.init();
-  }
 })();
